849cd2d08b6a412c2564594cc746069e5e036f72
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-03 12:57:30 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from "./App";
 
-
 test("renders without errors", ()=>{
     render(<App />);
+});
+
+test("renders the app header", ()=> {
+    //Arrange: Setup our react component
+    render(<App />);
+
+    //Act: Execute our behavior
+    //Assert: Test our app response
 });
