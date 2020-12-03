a93ef80858e74948cf4b40d45f4398030e24c4f7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-03 13:56:28 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -30,6 +30,6 @@ test("user can fill out and submit form", ()=> {
     
     //Assert: Test to see is submit worked
     //  1. Find our submitted species name
-    const newAnimal = screen.queryByText(/doggie/i);
+    const newAnimal = screen.queryByText(/canine/i);
     expect(newAnimal).toBeInTheDocument();
 });
