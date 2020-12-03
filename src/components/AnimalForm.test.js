442c136e6f9fbbf4e19632d1e2a72f191f462944
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-12-03 13:36:54 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -6,3 +6,16 @@ test("renders without errors", ()=>{
     render(<AnimalForm />);
 });
 
+test("user can fill out and submit form", ()=> {
+    //Arrange: setup react component
+    render(<AnimalForm />);
+
+    //Act: Submit our form:
+    //  1. Query for our form elements.
+    //  2. Type into our form elements.
+    //  3. Query for our form button.
+    //  4. Click our form button.
+
+    //Assert: Test to see is submit worked
+    //  1. Find our submitted species name
+});
